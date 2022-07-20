e334c0d8699067fa3995c7c77ce12fed3dea7078
AlxndrThOk <106681822+AlxndrThOk@users.noreply.github.com>
2022-07-20 16:29:02 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Random;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Random rnd = new Random();
+
+        int [] dataPoints = new int[100];
+        double total = 0;
+        double average;
+
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            dataPoints[i] = rnd.nextInt(100) + 1;
+            System.out.print(dataPoints[i] + " | ");
+            total += dataPoints[i];
+        }
+        average = total / 100;
+
+        System.out.println("\nthe total is " + total + "\nthe average is " + average);
     }
 }
